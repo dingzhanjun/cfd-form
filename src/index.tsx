a5162c73c7d73aeebc6211dc7e8d158820bf4639
John Ding <zhanjun.ding@gm.com>
2023-10-03 14:41:30 -0400

    fix issue in app.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client'
-import "./styles/tailwind.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import React from 'react';
+import './styles/tailwind.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -13,4 +13,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
