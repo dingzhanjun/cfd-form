aaae5362be106522f2a846b5ccdf973b6ff1de99
John Ding <zhanjun.ding@gm.com>
2023-10-03 12:46:17 -0400

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-red-300 bg-gradient-to-br from-gray-300 via-teal-700 to-gray-800">
+      <div className="flex items-center animate-bounce">CFD Form App</div>
+    </div>
+  )
+}
+
+export default App
